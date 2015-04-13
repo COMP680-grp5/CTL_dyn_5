4878b43834d6c173fb89bdb9476fe4665d620e00
Thomas Perez <thomasperez@Thomass-MacBook-Pro.local>
2015-04-13 10:50:56 -0700

    Initial Commit

---
+++ CTL_dyn_5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CTL_dyn_5
+//
+//  Created by Thomas Perez on 4/13/15.
+//  Copyright (c) 2015 Thomas Perez. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
